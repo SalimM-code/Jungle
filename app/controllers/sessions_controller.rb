e7ff0f900aed2ab1d8698d7707fa60dee75bc274
salimM-code <mustafasalim000@gmail.com>
2021-11-11 15:00:34 -0500

    fix the user auth bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
-      session[:user_id] = user.id
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/login'
@@ -12,7 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    session.delete(:user_id)
     redirect_to '/login'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
@@ -20,8 +26,7 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
-  resources :users, only:[:create, :new]
-  resources :sessions, only: [:create, :new, :destroy]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
